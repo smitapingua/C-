e65a92b9f51f94271526e0acd29194c55f079946
smitapingua <33260271+smitapingua@users.noreply.github.com>
2017-10-31 21:08:09 +0530

    Add files via upload

---
+++ powrec.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+float powr(float x,int y)
+{
+    if(y==0)
+        return 1.0;
+    else
+        return (x*powr(x,y-1));
+}
+
+int main()
+{
+    float b;
+    int r;
+    cout<<"Enter the required no.:";
+    cin>>b;
+    cout<<"\nEnter the power to be calculated:";
+    cin>>r;
+    float ans;
+    if(r>=0)
+    {
+        ans=powr(b,r);
+        cout<<"\nThe result of "<<b<<"^"<<r<<" is:"<<ans;
+    }
+    if(r<0)
+    {
+        ans=powr(b,-r);
+        cout<<"\nThe result of "<<b<<"^"<<r<<" is:1/"<<ans;
+    }
+    getch();
+    return 0;
+}
